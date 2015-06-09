3f57563f94e6e6259b2aa0e46feabeec97918f36
Florian Krüger <florian.krueger@projectserver.org>
2015-06-09 22:20:33 +0100

    added SHOUTCLOUD colors

--- SHOUTCLOUD/Constants.swift
+++ SHOUTCLOUD/Constants.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 public struct Constants {
   public static let errorDomain = "io.shoutcloud.error"
@@ -14,5 +15,12 @@ public struct Constants {
   public enum ErrorCodes: Int {
     case InvalidRequest = 1
     case InvalidResponse = 2
+  }
+
+  public struct Colors {
+    static let black = UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 1.0)
+    static let red = UIColor(red: 226/255, green: 98/255, blue: 98/255, alpha: 1.0)
+    static let green = UIColor(red: 0/255, green: 157/255, blue: 144/255, alpha: 1.0)
+    static let blue = UIColor(red: 0/255, green: 123/255, blue: 160/255, alpha: 1.0)
   }
 }
