7ca2cfb0ac9c0ebbc82da90196170391c65a8015
Florian Krüger <florian.krueger@projectserver.org>
2015-06-09 21:51:51 +0100

    added EXAMPLE

---
+++ EXAMPLE/User Interface/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EXAMPLE
+//
+//  Created by Florian Krüger on 03/06/15.
+//  Copyright (c) 2015 projectserver.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
